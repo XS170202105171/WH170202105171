8090f0e154fac93d29b7a9bff99f70f9f5d20975
Administrator <Administrator@MD502-88>
2019-05-15 09:04:59 +0800

    111

--- src/demoProj/jiaowu.java
+++ src/demoProj/jiaowu.java
@@ -1,6 +1,6 @@
        package demoProj;
 			public class jiaowu {
 				public static void main(String[] args) {
-					System.out.println("good branch!!");
+					System.out.println("good branch!");
 				}
 }
